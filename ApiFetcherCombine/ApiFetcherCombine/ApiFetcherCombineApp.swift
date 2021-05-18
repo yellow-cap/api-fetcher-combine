606f111f8f74d8b03390545ef1dc85c12a2e7bb1
Artem <artem@wildapricot.com>
2021-05-18 09:13:05 +0300

    add project

---
+++ ApiFetcherCombine/ApiFetcherCombine/ApiFetcherCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiFetcherCombineApp.swift
+//  ApiFetcherCombine
+//
+//  Created by Artem on 18.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct ApiFetcherCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApiFetcherCombine/ApiFetcherCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ApiFetcherCombine
+//
+//  Created by Artem on 18.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
